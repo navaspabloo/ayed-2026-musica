7025fd97c4a0a6c06b9ff6aa988e9e1c7dce6b68
navaspabloo <navaspabloo@gmail.com>
2026-09-11 21:02:56 -0300

    add: lista de canciones

---
+++ src/dominio/datos.py
@@ -0,0 +1,47 @@
+canciones = [
+    {
+        "id": 1,
+        "titulo": "De Música Ligera",
+        "artista": "Soda Stereo",
+        "album": "Canción Animal",
+        "genero": "Rock Nacional",
+        "anio": 1990,
+        "duracion_seg": 118
+    },
+    {
+        "id": 2,
+        "titulo": "Muchacha (Ojos de Papel)",
+        "artista": "Almendra",
+        "album": "Almendra",
+        "genero": "Rock Nacional",
+        "anio": 1969,
+        "duracion_seg": 162
+    },
+    {
+        "id": 3,
+        "titulo": "Ji Ji Ji",
+        "artista": "Patricio Rey y sus Redonditos de Ricota",
+        "album": "Oktubre",
+        "genero": "Rock Nacional",
+        "anio": 1986,
+        "duracion_seg": 299
+    },
+    {
+        "id": 4,
+        "titulo": "Mil Horas",
+        "artista": "Los Abuelos de la Nada",
+        "album": "Vasos y Besos",
+        "genero": "Rock Nacional",
+        "anio": 1983,
+        "duracion_seg": 189
+    },
+    {
+        "id": 5,
+        "titulo": "Persiana Americana",
+        "artista": "Soda Stereo",
+        "album": "Signos",
+        "genero": "Rock Nacional",
+        "anio": 1986,
+        "duracion_seg": 321
+    }
+]
